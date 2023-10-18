64188f2278cf5ff67e5394212c83dd2d382b4fe4
Md. Mosfikur Rahman <mdmosfikurrahman.cse@gmail.com>
2023-10-18 10:24:34 +0600

    Lots of BGMEA and BKMEA

--- src/main/java/org/epde/bin/BinFilter.java
+++ src/main/java/org/epde/bin/BinFilter.java
@@ -17,28 +17,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BinFilter {
+    private static final String BGMEA_AUTH_API_URL = "http://182.160.98.85:90/api/v1/token/access";
+    private static final String BGMEA_API_URL = "http://182.160.98.85:90/api/v1/udam/infos";
+    private static final String BKMEA_API_URL = "http://20.205.119.208:90/api/DocumentList";
+    private static final String CLIENT_ID = "36aab894-f11b-42cd-8874-8797e0a3f062";
+    private static final String CLIENT_SECRET = "7487466ac007d96c3c246291d1fc2d54884a8349f83a64a0c75cade982173214";
+    private static final String START_DATE = "2022-07-01";
+    private static final String END_DATE = "2023-09-01";
+
     public static void main(String[] args) {
-
-        String startDate = "2022-07-01";
-        String endDate = "2023-09-01";
-
         List<String> bgmeaBins = new ArrayList<>();
         List<String> bkmeaBins = new ArrayList<>();
         List<String> invalidBins = new ArrayList<>();
 
-        String jsonFilePath = "E:/Project/JavaBasics/src/main/resources/new_combined_bins/new_combined_bins.json";
+        // Attempt to obtain the bearer token
+        String bearerToken = getBearerToken();
+        if (bearerToken == null) {
+            System.err.println("Failed to obtain the bearer token. Exiting.");
+            return;
+        }
 
-        String bgmeaAuthApiUrl = "http://182.160.98.85:90/api/v1/token/access";
-        String bgmeaApiUrl = "http://182.160.98.85:90/api/v1/udam/infos";
-        String bkmeaApiUrl = "http://20.205.119.208:90/api/DocumentList";
+        try {
+            // Load JSON data from the file
+            JSONObject inputJson = loadJsonFromFile();
 
-        String requestBody = "{\"clientId\": \"36aab894-f11b-42cd-8874-8797e0a3f062\", \"clientSecret\": \"7487466ac007d96c3c246291d1fc2d54884a8349f83a64a0c75cade982173214\"}";
+            JSONArray binData = inputJson.getJSONArray("binData");
 
-        String accessToken = null;
+            for (int i = 0; i < binData.length(); i++) {
+                String bin = binData.getString(i);
+                String bgmeaUrl = createBgmeaApiUrl(bin);
+                String bkmeaUrl = createBkmeaApiUrl(bin);
+
+                if (checkBinAndAdd(bgmeaUrl, bearerToken, bgmeaBins, bin) || checkBinAndAdd(bkmeaUrl, null, bkmeaBins, bin)) {
+                    continue;
+                }
+
+                invalidBins.add(bin);
+            }
+
+            // Save results to JSON files
+            saveToJsonFile(bgmeaBins, "new_bgmea_bins.json");
+            saveToJsonFile(bkmeaBins, "new_bkmea_bins.json");
+            saveToJsonFile(invalidBins, "invalid_bins.json");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total BINS: " + (bkmeaBins.size() + bgmeaBins.size() + invalidBins.size()));
+    }
+
+    private static String createBgmeaApiUrl(String bin) {
+        return BGMEA_API_URL + "?BIN=" + bin + "&StartDate=" + START_DATE + "&EndDate=" + END_DATE;
+    }
+
+    private static String createBkmeaApiUrl(String bin) {
+        return BKMEA_API_URL + "?binNo=" + bin + "&startDate=" + START_DATE + "&endDate=" + END_DATE;
+    }
+
+    private static boolean checkBinAndAdd(String apiUrl, String token, List<String> binList, String bin) {
+        boolean isBin = (token != null
+                ? checkBgmeaBin(apiUrl, token)
+                : checkBkmeaBin(apiUrl));
+        if (isBin) {
+            binList.add(bin);
+        }
+        return isBin;
+    }
+
+    private static String getBearerToken() {
+        String requestBody = "{\"clientId\": \"" + CLIENT_ID + "\", \"clientSecret\": \"" + CLIENT_SECRET + "\"}";
 
         try {
             HttpClient httpClient = HttpClients.createDefault();
-            HttpPost httpPost = new HttpPost(bgmeaAuthApiUrl);
+            HttpPost httpPost = new HttpPost(BGMEA_AUTH_API_URL);
 
             httpPost.setHeader("Content-Type", "application/json");
             httpPost.setEntity(new StringEntity(requestBody));
@@ -46,116 +98,89 @@ public class BinFilter {
             HttpResponse response = httpClient.execute(httpPost);
 
             if (response.getStatusLine().getStatusCode() == 200) {
-                String responseJson = EntityUtils.toString(response.getEntity());
-                JSONObject jsonResponse = new JSONObject(responseJson);
-
-                accessToken = jsonResponse.getString("accessToken");
+                JSONObject jsonResponse = new JSONObject(EntityUtils.toString(response.getEntity()));
+                return jsonResponse.getString("accessToken");
+            } else {
+                System.err.println("Failed to obtain the bearer token. Status code: " + response.getStatusLine().getStatusCode());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
+    }
 
-        String bearerToken = accessToken;
-
-        try {
-            File file = new File(jsonFilePath);
-            FileReader fileReader = new FileReader(file);
+    private static JSONObject loadJsonFromFile() throws IOException {
+        try (FileReader fileReader = new FileReader("E:/Project/JavaBasics/src/main/resources/new_combined_bins/new_combined_bins.json")) {
             JSONTokener jsonTokener = new JSONTokener(fileReader);
-            JSONObject inputJson = new JSONObject(jsonTokener);
+            return new JSONObject(jsonTokener);
+        }
+    }
 
-            JSONArray binData = inputJson.getJSONArray("binData");
+    private static boolean checkBgmeaBin(String bgmeaUrl, String bearerToken) {
+        try {
+            URL bgmeaUrlForCheck = new URL(bgmeaUrl);
+            HttpURLConnection bgmeaConnection = (HttpURLConnection) bgmeaUrlForCheck.openConnection();
+            bgmeaConnection.setRequestMethod("GET");
 
-            for (int i = 0; i < binData.length(); i++) {
-                String bin = binData.getString(i);
+            bgmeaConnection.setRequestProperty("Authorization", "Bearer " + bearerToken);
 
-                String bgmeaUrl = bgmeaApiUrl + "?BIN=" + bin + "&StartDate=" + startDate + "&EndDate=" + endDate;
+            if (bgmeaConnection.getResponseCode() == 200) {
+                JSONObject bgmeaJsonResponse = getJsonObject(bgmeaConnection);
 
-                URL bgmeaUrlForCheck = new URL(bgmeaUrl);
-                HttpURLConnection bgmeaConnection = (HttpURLConnection) bgmeaUrlForCheck.openConnection();
-                bgmeaConnection.setRequestMethod("GET");
-
-                bgmeaConnection.setRequestProperty("Authorization", "Bearer " + bearerToken);
-
-                if (bgmeaConnection.getResponseCode() == 200) {
-                    JSONObject bgmeaJsonResponse = getJsonObject(bgmeaConnection);
-
-                    if (bgmeaJsonResponse.has("statusCode") && bgmeaJsonResponse.getInt("statusCode") == 200) {
-                        if (!bgmeaJsonResponse.getJSONObject("data").isNull("membershipId")) {
-                            bgmeaBins.add(bin);
-                        }
-                        else {
-                            String bkmeaUrl = bkmeaApiUrl + "?binNo=" + bin + "&startDate=" + startDate + "&endDate=" + endDate;
-                            URL bkmeaUrlForCheck = new URL(bkmeaUrl);
-                            HttpURLConnection bkmeaConnection = (HttpURLConnection) bkmeaUrlForCheck.openConnection();
-                            bkmeaConnection.setRequestMethod("GET");
-
-                            if (bkmeaConnection.getResponseCode() != 500) {
-                                JSONObject bkmeaJsonResponse = getJsonObject(bkmeaConnection);
-
-                                if (bkmeaJsonResponse.has("regNo")){
-                                    bkmeaBins.add(bin);
-                                }
-
-                            } else {
-                                invalidBins.add(bin);
-                            }
-                            bkmeaConnection.disconnect();
-                        }
+                if (bgmeaJsonResponse.has("statusCode") && bgmeaJsonResponse.getInt("statusCode") == 200) {
+                    if (!bgmeaJsonResponse.getJSONObject("data").isNull("membershipId")) {
+                        return true;
                     }
-                    bgmeaConnection.disconnect();
                 }
             }
-
-            JSONObject updatedBgmeaBinJson = new JSONObject();
-            JSONObject updatedBkmeaBinJson = new JSONObject();
-            JSONObject invalidBinJson = new JSONObject();
-
-            updatedBgmeaBinJson.put("binData", new JSONArray(bgmeaBins));
-            updatedBkmeaBinJson.put("binData", new JSONArray(bkmeaBins));
-            invalidBinJson.put("binData", new JSONArray(invalidBins));
-
-            String updatedBgmeaJsonFilePath = "E:/Project/JavaBasics/src/main/resources/new_combined_bins/new_bgmea_bins.json";
-            String updatedBkmeaJsonFilePath = "E:/Project/JavaBasics/src/main/resources/new_combined_bins/new_bkmea_bins.json";
-            String invalidBinFilePath = "E:/Project/JavaBasics/src/main/resources/new_combined_bins/invalid_bins.json";
-
-            try (FileWriter fileWriter = new FileWriter(updatedBgmeaJsonFilePath)) {
-                updatedBgmeaBinJson.write(fileWriter);
-                System.out.println("Total " + bgmeaBins.size() + " BGMEA BINs saved in " + updatedBgmeaJsonFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try (FileWriter fileWriter = new FileWriter(updatedBkmeaJsonFilePath)) {
-                updatedBkmeaBinJson.write(fileWriter);
-                System.out.println("Total " + bkmeaBins.size() + " BKMEA BINs saved in " + updatedBkmeaJsonFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try (FileWriter fileWriter = new FileWriter(invalidBinFilePath)) {
-                invalidBinJson.write(fileWriter);
-                System.out.println("Total " + invalidBins.size() + " INVALID BINs saved in " + invalidBinFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
+    }
 
-        System.out.println("Total BINS: " + (bkmeaBins.size() + bgmeaBins.size() + invalidBins.size()));
+    private static boolean checkBkmeaBin(String bkmeaUrl) {
+        try {
+            URL bkmeaUrlForCheck = new URL(bkmeaUrl);
+            HttpURLConnection bkmeaConnection = (HttpURLConnection) bkmeaUrlForCheck.openConnection();
+            bkmeaConnection.setRequestMethod("GET");
+
+            if (bkmeaConnection.getResponseCode() != 500) {
+                JSONObject bkmeaJsonResponse = getJsonObject(bkmeaConnection);
+
+                if (bkmeaJsonResponse.has("regNo")) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static void saveToJsonFile(List<String> bins, String filePath) {
+        JSONObject binJson = new JSONObject();
+        binJson.put("binData", new JSONArray(bins));
+
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            binJson.write(fileWriter);
+            System.out.println("Total " + bins.size() + " BINs saved in " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static JSONObject getJsonObject(HttpURLConnection conn) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String line;
+        try (InputStream inputStream = conn.getInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder response = new StringBuilder();
+            String line;
 
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            return new JSONObject(response.toString());
         }
-        reader.close();
-
-        return new JSONObject(response.toString());
     }
 }
