da881772c0af131c795f4bb63bc9c0a9958f97e1
Md. Mosfikur Rahman <mdmosfikurrahman.cse@gmail.com>
2023-10-25 13:42:07 +0600

    Lots of BGMEA and BKMEA

---
+++ src/main/java/org/epde/ProjectStructureAnalyzer.java
@@ -0,0 +1,36 @@
+package org.epde;
+
+import java.io.File;
+
+public class ProjectStructureAnalyzer {
+
+    public static void main(String[] args) {
+        String projectPath = "F:\\CBMS\\Data Fetcher\\data-fetcher\\src";
+        File projectDirectory = new File(projectPath);
+
+        if (projectDirectory.exists() && projectDirectory.isDirectory()) {
+            listProjectStructure(projectDirectory, 0);
+        } else {
+            System.out.println("The specified project path does not exist or is not a directory.");
+        }
+    }
+
+    public static void listProjectStructure(File directory, int indentLevel) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.println(getIndentation(indentLevel) + "- " + file.getName());
+                    listProjectStructure(file, indentLevel + 1);
+                } else {
+                    System.out.println(getIndentation(indentLevel) + "- " + file.getName());
+                }
+            }
+        }
+    }
+
+    public static String getIndentation(int indentLevel) {
+        return "  ".repeat(Math.max(0, indentLevel));
+    }
+}
+
